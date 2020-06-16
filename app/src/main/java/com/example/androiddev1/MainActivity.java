d57ace7f94a719f1f1cb5c926252ebe6ae4882f5
James Ware <james-ware@hotmail.co.uk>
2020-06-16 09:59:37 +0100

    After adding buttons, header, toast

--- app/src/main/java/com/example/androiddev1/MainActivity.java
+++ app/src/main/java/com/example/androiddev1/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.androiddev1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView firstTextView = (TextView) findViewById(R.id.pick_menu_header);
+        Button ambientButton = (Button) findViewById(R.id.button_ambient);
+        ambientButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firstTextView.setText("You clicked");
+            }
+        });
+
+    }
+
+    public void getAmbientList(View v) {
+        Toast myToast = Toast.makeText(getApplicationContext(),
+                "Retrieving Ambient Picklist", Toast.LENGTH_LONG);
+        myToast.show();
+        Log.d("success", "Toast shown");
     }
 }
